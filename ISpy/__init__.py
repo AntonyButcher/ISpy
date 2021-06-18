0b90f9f91a4e7bb0fceb39c139a4ea3f10261520
AntonyButcher <antony.butcher@bristol.ac.uk>
2021-06-18 15:56:00 +0100

    file update

---
+++ ISpy/__init__.py
@@ -0,0 +1,7 @@
+name = "ISpy"
+__version__ = "0.0.1"
+__description__ = "ISpy - A package for detecting and processing induced seismicity."
+__license__ = "MIT"
+__author__ = "Antony Butcher"
+__email__ = ""
+import ISpy.model.sens
